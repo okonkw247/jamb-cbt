87f8b3774ff22eca5649a8395c421bc30a59d4c7
jamb-cbt <okonkw247@gmail.com>
2026-02-23 02:22:31 +0100

    fix firebase db export

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,5 +1,6 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA-xldRxhFqOsxv3xnjWsaOmcFG-nqG1n4",
@@ -8,8 +9,10 @@ const firebaseConfig = {
   storageBucket: "jamb-cbt-8fa5d.firebasestorage.app",
   messagingSenderId: "136310165119",
   appId: "1:136310165119:web:9549495df4da9ceda5b94a",
+  databaseURL: "https://jamb-cbt-8fa5d-default-rtdb.firebaseio.com",
 };
 
-const app = initializeApp(firebaseConfig);
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 export const auth = getAuth(app);
+export const db = getDatabase(app);
 export default app;
