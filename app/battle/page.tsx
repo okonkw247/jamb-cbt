90c195e8e06c827d31e379914d3bb037b8a783b9
jamb-cbt <okonkw247@gmail.com>
2026-03-01 23:02:21 +0100

    fix chatMessages to messages

--- app/battle/page.tsx
+++ app/battle/page.tsx
@@ -956,7 +956,7 @@ const removePlayer = async (pid: string) => {
               onClick={() => {
                 setShowChat(!showChat);
                 setUnreadCount(0);
-                setLastSeenCount(chatMessages.length);
+                setLastSeenCount(messages.length);
               }}
               className="relative bg-blue-500 text-white rounded-2xl shadow-lg flex flex-col items-center justify-center px-3 py-2 gap-0.5"
             >
