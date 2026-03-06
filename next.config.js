3b1d3353c29acdae49c81dab7f951d9b2810dc58
jamb-cbt <okonkw247@gmail.com>
2026-03-07 00:09:01 +0100

    fix sitemap.xml content type headers

--- next.config.js
+++ next.config.js
@@ -1,5 +1,28 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/sitemap.xml',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/xml',
+          },
+        ],
+      },
+      {
+        source: '/robots.txt',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'text/plain',
+          },
+        ],
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
